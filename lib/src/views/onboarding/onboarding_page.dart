059b66471823bc5c31ae35473a81c5f824dcdcaf
Keep Going Solutions <dev@keepgoing.solutions>
2024-10-31 15:26:47 -0400

    fix issue with onboarding for small screen

--- lib/src/views/onboarding/onboarding_page.dart
+++ lib/src/views/onboarding/onboarding_page.dart
@@ -28,158 +28,158 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            if (title != null)
-              Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      top: 40.0), // Adjust the top padding as needed
-                  child: SizedBox(
-                    width: MediaQuery.of(context)
-                        .size
-                        .width, // Make width stretch across the screen
-                    height: 70.0, // Set a fixed height for the text box
-                    child: FittedBox(
-                      fit: BoxFit.fitWidth,
-                      child: Text(
-                        title!,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w700, // Bold text
-                          color: Colors.white, // Color of the text
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              if (title != null)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 1.0), // Adjust the top padding as needed
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 20.0,
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          title!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700, // Bold text
+                            color: Colors.white, // Color of the text
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        textAlign: TextAlign.center,
                       ),
                     ),
                   ),
                 ),
-              ),
-            if (title != null)
-              const SizedBox(
-                height: 50,
-              ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (title != null) Image.asset(imageAsset, height: 200),
-                if (title == null) Image.asset(imageAsset, height: 300),
-                SizedBox(height: 50),
-                if (title != null)
-                  Text(
-                    subtitle,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700, // Bold text
-                      fontSize: 20,
-                      color: Colors.white, // Color of the text
-                    ),
-                  ),
-                if (title != null) SizedBox(height: 30),
-                if (title != null)
-                  Text(
-                    description,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.white, // Color of the text
-                    ),
-                  ),
-              ],
-            ),
-            if (title != null) SizedBox(height: 90),
-            if (title != null)
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 368),
-                child: ElevatedButton(
-                  onPressed: onButtonPressed,
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 48, vertical: 16),
-                    child: Text(
-                      buttonText,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            if (title == null)
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                decoration: BoxDecoration(
-                  color: const Color(0xFF2c2b2b),
-                  borderRadius: BorderRadius.circular(
-                      10.0), // Adds rounded corners to the container
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(height: 20),
+              if (title != null) const SizedBox(height: 30),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (title != null) Image.asset(imageAsset, height: 200),
+                  if (title == null) Image.asset(imageAsset, height: 200),
+                  const SizedBox(height: 50),
+                  if (title != null)
                     Text(
                       subtitle,
                       style: const TextStyle(
+                        fontWeight: FontWeight.w700, // Bold text
                         fontSize: 20,
+                        color: Colors.white, // Color of the text
                       ),
                     ),
-                    if (subtitle2 != null)
-                      Text(
-                        subtitle2!,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w700, // Bold text
-                          fontSize: 20,
-                        ),
-                      ),
-                    SizedBox(height: 30),
+                  if (title != null) const SizedBox(height: 30),
+                  if (title != null)
                     Text(
                       description,
                       textAlign: TextAlign.center,
                       style: const TextStyle(
-                        fontSize: 16,
+                        fontSize: 20,
                         color: Colors.white, // Color of the text
                       ),
                     ),
-                    SizedBox(height: 30),
-                    ConstrainedBox(
-                      constraints: const BoxConstraints(maxWidth: 368),
-                      child: ElevatedButton(
-                        onPressed: onButtonPressed,
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 48, vertical: 16),
-                          child: Text(
-                            buttonText,
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ),
+                ],
+              ),
+              if (title != null) const SizedBox(height: 90),
+              if (title != null)
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 368),
+                  child: ElevatedButton(
+                    onPressed: onButtonPressed,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 16),
+                      child: Text(
+                        buttonText,
+                        style: const TextStyle(fontSize: 18),
                       ),
                     ),
-                    if (secondaryButtonText != null)
+                  ),
+                ),
+              if (title == null)
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF2c2b2b),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(height: 20),
+                      Text(
+                        subtitle,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      if (subtitle2 != null)
+                        Text(
+                          subtitle2!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      const SizedBox(height: 30),
+                      Text(
+                        description,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
                       ConstrainedBox(
                         constraints: const BoxConstraints(maxWidth: 368),
-                        child: OutlinedButton(
-                          onPressed: onSecondaryButtonPressed,
-                          style: OutlinedButton.styleFrom(
+                        child: ElevatedButton(
+                          onPressed: onButtonPressed,
+                          style: ElevatedButton.styleFrom(
                             minimumSize: const Size.fromHeight(50),
                           ),
-                          child: Text(
-                            secondaryButtonText!,
-                            style: TextStyle(fontSize: 18),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 16),
+                            child: Text(
+                              buttonText,
+                              style: const TextStyle(fontSize: 18),
+                            ),
                           ),
                         ),
                       ),
-                    SizedBox(height: 30),
-                  ],
+                      if (secondaryButtonText != null)
+                        const SizedBox(height: 10),
+                      if (secondaryButtonText != null)
+                        ConstrainedBox(
+                          constraints: const BoxConstraints(maxWidth: 368),
+                          child: OutlinedButton(
+                            onPressed: onSecondaryButtonPressed,
+                            style: OutlinedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(50),
+                            ),
+                            child: Text(
+                              secondaryButtonText!,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
